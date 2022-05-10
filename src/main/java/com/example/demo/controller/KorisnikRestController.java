cd1289a40bccdc76cfcf8ee800895a02213e3094
anatadic <anatadic111@gmail.com>
2022-05-10 20:26:12 +0200

    added korisnik rest controller

---
+++ src/main/java/com/example/demo/controller/KorisnikRestController.java
@@ -0,0 +1,109 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.KorisnikDto;
+import com.example.demo.dto.LoginDto;
+import com.example.demo.dto.RegisterDto;
+import com.example.demo.entity.Korisnik;
+import com.example.demo.entity.PolEnum;
+import com.example.demo.entity.UlogaEnum;
+import com.example.demo.service.KorisnikService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class KorisnikRestController {
+
+    @Autowired
+    private KorisnikService korisnikService;
+
+    @PostMapping("/api/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+
+        if (registerDto.getKorisnickoIme().isEmpty() || registerDto.getLozinka().isEmpty()
+                || registerDto.getIme().isEmpty() || registerDto.getPrezime().isEmpty() ||
+                registerDto.getUloga().isEmpty() || registerDto.getPol().isEmpty()) {
+            return new ResponseEntity("Neispravno uneti podaci", HttpStatus.BAD_REQUEST);
+        }
+
+        if (korisnikService.getByKorisnickoIme(registerDto.getKorisnickoIme()) != null) {
+            return new ResponseEntity("Korisnicko ime vec postoji", HttpStatus.BAD_REQUEST);
+        }
+
+        Korisnik korisnik = new Korisnik();
+        korisnik.setKorisnickoIme(registerDto.getKorisnickoIme());
+        korisnik.setIme(registerDto.getIme());
+        korisnik.setPrezime(registerDto.getPrezime());
+        korisnik.setLozinka(registerDto.getLozinka());
+        korisnik.setPol(PolEnum.valueOf(registerDto.getPol()));
+        korisnik.setDatumRodjenja(new Date(registerDto.getDatumRodjenja()));
+        korisnik.setUloga(UlogaEnum.valueOf(registerDto.getUloga()));
+
+        korisnikService.registerKorisnik(korisnik);
+
+        return ResponseEntity.ok("Uspesna registracija!");
+    }
+
+    @PostMapping("/api/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto, HttpSession session) {
+
+        if (loginDto.getKorisnickoIme().isEmpty() || loginDto.getLozinka().isEmpty()) {
+            return new ResponseEntity("Neispravno uneti podaci", HttpStatus.BAD_REQUEST);
+        }
+
+        Korisnik loggedKorisnik = korisnikService.login(loginDto.getKorisnickoIme(), loginDto.getLozinka());
+
+        if (loggedKorisnik == null) {
+            return new ResponseEntity<>("Korisnik ne postoji!", HttpStatus.NOT_FOUND);
+        }
+
+        session.setAttribute("korisnik", loggedKorisnik);
+        return ResponseEntity.ok("Uspesno logovanje!");
+    }
+
+    @GetMapping("/api/korisnici/{id}")
+    public Korisnik getKorisnik(@PathVariable(name = "id") Long id, HttpSession session) {
+        Korisnik korisnik = (Korisnik) session.getAttribute("korisnik");
+        session.invalidate();
+        return korisnikService.findOne(id);
+    }
+
+    @GetMapping("/api/korisnici")
+    public ResponseEntity<List<KorisnikDto>> getKorisnici(HttpSession session) {
+        List<Korisnik> korisnici = korisnikService.findAll();
+
+        Korisnik loggedKorisnik = (Korisnik) session.getAttribute("korisnik");
+        if (loggedKorisnik == null) {
+            System.out.println("Nema sesije");
+        }
+
+        List<KorisnikDto> dtos = new ArrayList<>();
+        for (Korisnik korisnik : korisnici) {
+            KorisnikDto dto = new KorisnikDto(korisnik.getKorisnickoIme(),
+                    korisnik.getIme(), korisnik.getPrezime(), korisnik.getPol().toString(),
+                    korisnik.getDatumRodjenja().toString(), korisnik.getUloga().toString());
+            dtos.add(dto);
+        }
+        return ResponseEntity.ok(dtos);
+    }
+
+    @PostMapping("/api/edit-korisnik")
+    public String editKorisnik(@RequestBody Korisnik korisnik) {
+        Korisnik editKorisnik = korisnikService.getByKorisnickoIme(korisnik.getKorisnickoIme());
+        editKorisnik.setIme(korisnik.getIme());
+        editKorisnik.setPrezime(korisnik.getPrezime());
+        editKorisnik.setDatumRodjenja(korisnik.getDatumRodjenja());
+        editKorisnik.setLozinka(korisnik.getLozinka());
+        editKorisnik.setPol(korisnik.getPol());
+        editKorisnik.setUloga(korisnik.getUloga());
+        this.korisnikService.save(editKorisnik);
+        return "Uspesno sacuvan korisnik!";
+    }
+
+}
---
+++ src/main/java/com/example/demo/dto/KorisnikDto.java
@@ -0,0 +1,76 @@
+package com.example.demo.dto;
+
+public class KorisnikDto {
+
+    private String korisnickoIme;
+
+    private String ime;
+
+    private String prezime;
+
+    private String pol;
+
+    private String datumRodjenja;
+
+    private String uloga;
+
+    public KorisnikDto() {
+    }
+
+    public KorisnikDto(String korisnickoIme, String ime, String prezime, String pol, String datumRodjenja, String uloga) {
+        this.korisnickoIme = korisnickoIme;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.pol = pol;
+        this.datumRodjenja = datumRodjenja;
+        this.uloga = uloga;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getPol() {
+        return pol;
+    }
+
+    public void setPol(String pol) {
+        this.pol = pol;
+    }
+
+    public String getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(String datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public String getUloga() {
+        return uloga;
+    }
+
+    public void setUloga(String uloga) {
+        this.uloga = uloga;
+    }
+}
